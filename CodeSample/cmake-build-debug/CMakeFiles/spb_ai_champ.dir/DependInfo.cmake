ecd726d5a8b93a4e08975e19ece2bc4b267d9731
ipipos56 <ipipos56@bk.ru>
2021-10-07 18:21:03 +0300

    initial commit

---
+++ CodeSample/cmake-build-debug/CMakeFiles/spb_ai_champ.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/MyStrategy.cpp" "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/cmake-build-debug/CMakeFiles/spb_ai_champ.dir/MyStrategy.cpp.obj"
+  "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/Stream.cpp" "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/cmake-build-debug/CMakeFiles/spb_ai_champ.dir/Stream.cpp.obj"
+  "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/TcpStream.cpp" "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/cmake-build-debug/CMakeFiles/spb_ai_champ.dir/TcpStream.cpp.obj"
+  "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/codegame/ClientMessage.cpp" "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/cmake-build-debug/CMakeFiles/spb_ai_champ.dir/codegame/ClientMessage.cpp.obj"
+  "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/codegame/ServerMessage.cpp" "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/cmake-build-debug/CMakeFiles/spb_ai_champ.dir/codegame/ServerMessage.cpp.obj"
+  "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/debugging/DebugCommand.cpp" "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/cmake-build-debug/CMakeFiles/spb_ai_champ.dir/debugging/DebugCommand.cpp.obj"
+  "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/main.cpp" "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/cmake-build-debug/CMakeFiles/spb_ai_champ.dir/main.cpp.obj"
+  "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/model/Action.cpp" "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/cmake-build-debug/CMakeFiles/spb_ai_champ.dir/model/Action.cpp.obj"
+  "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/model/Building.cpp" "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/cmake-build-debug/CMakeFiles/spb_ai_champ.dir/model/Building.cpp.obj"
+  "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/model/BuildingAction.cpp" "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/cmake-build-debug/CMakeFiles/spb_ai_champ.dir/model/BuildingAction.cpp.obj"
+  "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/model/BuildingProperties.cpp" "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/cmake-build-debug/CMakeFiles/spb_ai_champ.dir/model/BuildingProperties.cpp.obj"
+  "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/model/BuildingType.cpp" "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/cmake-build-debug/CMakeFiles/spb_ai_champ.dir/model/BuildingType.cpp.obj"
+  "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/model/DebugData.cpp" "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/cmake-build-debug/CMakeFiles/spb_ai_champ.dir/model/DebugData.cpp.obj"
+  "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/model/FlyingWorkerGroup.cpp" "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/cmake-build-debug/CMakeFiles/spb_ai_champ.dir/model/FlyingWorkerGroup.cpp.obj"
+  "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/model/Game.cpp" "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/cmake-build-debug/CMakeFiles/spb_ai_champ.dir/model/Game.cpp.obj"
+  "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/model/MoveAction.cpp" "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/cmake-build-debug/CMakeFiles/spb_ai_champ.dir/model/MoveAction.cpp.obj"
+  "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/model/Planet.cpp" "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/cmake-build-debug/CMakeFiles/spb_ai_champ.dir/model/Planet.cpp.obj"
+  "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/model/Player.cpp" "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/cmake-build-debug/CMakeFiles/spb_ai_champ.dir/model/Player.cpp.obj"
+  "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/model/Resource.cpp" "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/cmake-build-debug/CMakeFiles/spb_ai_champ.dir/model/Resource.cpp.obj"
+  "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/model/WorkerGroup.cpp" "C:/Users/ipipos56/Desktop/AI CUP/CodeSample/cmake-build-debug/CMakeFiles/spb_ai_champ.dir/model/WorkerGroup.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "WIN32"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ MyCode/cmake-build-debug/CMakeFiles/spb_ai_champ.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for spb_ai_champ.
+# This may be replaced when dependencies are built.
